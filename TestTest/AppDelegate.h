bbf5f044e31c22bf4e99235b2bcfae4c44002fb4
Ina Roesjorde <inaros@knowit.no>
2015-11-30 13:22:05 +0100

    Initial Commit

---
+++ TestTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTest
+//
+//  Created by Ina Roesjorde on 30/11/15.
+//  Copyright © 2015 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTest
+//
+//  Created by Ina Roesjorde on 30/11/15.
+//  Copyright © 2015 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
